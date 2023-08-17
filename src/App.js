dc13a0618d5a50087e33b81680de4fd4a8a9feae
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-08-17 16:20:58 +1000

    retrieve data with question and answer, add some css style

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from "react"
 import Quiz from "./Component/Quiz"
 export default function App() {
 
-  const [questionList, setQuestionList]=React.useState({})
+  const [questionList, setQuestionList]=React.useState([]);
   React.useEffect(()=>{
-    fetch("https://opentdb.com/api.php?amount=5")
+    fetch("https://opentdb.com/api.php?amount=5&type=multiple")
       .then(res=>res.json())
-      .then(data=>setQuestionList(data));
+      .then(data=>setQuestionList(data.results));
+
   },[]);
   console.log(questionList);
+  const questionElement=questionList.map((question, index)=><Quiz key={index} question={question} correctAnswer={question.correct_answer} />)
   return (
     <div className="App">
-      <Quiz />
+      {questionElement}
+      <button>Next</button>
     </div>
   );
 }
--- src/Component/Quiz.js
+++ src/Component/Quiz.js
@@ -1,11 +1,32 @@
 import React from "react";
+import Answer from "./Answer";
 
-export default function Quiz(){
+export default function Quiz(props){
+    let allAnswer=props.question.incorrect_answers;
+  
+
+    allAnswer.push(props.correctAnswer);
+    console.log(allAnswer);
+    function generateRandomAnswer(){
+
+        for(let i=allAnswer.length-1; i>0; i--){
+            let j=Math.floor(Math.random()*(i+1));
+            [allAnswer[i], allAnswer[j]]=[allAnswer[j], allAnswer[i]];
+        }
+        return allAnswer;
+    }
+    allAnswer=generateRandomAnswer()
+    const answerElement=allAnswer.map((answer, index)=><Answer key={index} answer={answer}/>)
     return (
+        <div>
         <div className="quiz">
-            <h1 className="quizz--question">Question:</h1>
-            <h3 className="quiz--answer">Answer:</h3>
-            <hr></hr>
+            <h2 className="quizz--question">Question: {props.question.question.replace(/&quot;/g, '"').replace(/&#039;/g,"'")}</h2>
+            <h4 className="quiz--answer">Answer:</h4>
+            <div>
+            {answerElement}
+            </div>
+        </div>
+        <hr></hr>
         </div>
     );
 }
