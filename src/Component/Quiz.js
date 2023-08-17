e74635372727de5bca9474218570e3d4f4a3daee
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2023-08-17 14:43:48 +1000

    Fetch API data with 5 question

---
+++ src/Component/Quiz.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Quiz(){
+    return (
+        <div className="quiz">
+            <h1 className="quizz--question">Question:</h1>
+            <h3 className="quiz--answer">Answer:</h3>
+            <hr></hr>
+        </div>
+    );
+}
