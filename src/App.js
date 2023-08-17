e0070d544a7988f6492fd5f3eff2e9c3386cbfab
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-08-17 14:43:48 +1000

    Fetch API data with 5 question

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Quiz from "./Component/Quiz"
+export default function App() {
 
-function App() {
+  const [questionList, setQuestionList]=React.useState({})
+  React.useEffect(()=>{
+    fetch("https://opentdb.com/api.php?amount=5")
+      .then(res=>res.json())
+      .then(data=>setQuestionList(data));
+  },[]);
+  console.log(questionList);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Quiz />
     </div>
   );
 }
 
-export default App;
