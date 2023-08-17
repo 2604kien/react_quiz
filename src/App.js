618a9db009358aff4ff0bd14765503168558db6a
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2023-08-17 18:35:30 +1000

    user can click next to generate new question and answer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React from "react"
 import Quiz from "./Component/Quiz"
 export default function App() {
-
+  const [reset, setReset]=React.useState(false);
   const [questionList, setQuestionList]=React.useState([]);
   const [point, setPoint]=React.useState(0);
+  
   React.useEffect(()=>{
     fetch("https://opentdb.com/api.php?amount=5&type=multiple")
       .then(res=>res.json())
       .then(data=>setQuestionList(data.results));
+      console.log(questionList)
+  },[reset]);  
 
-  },[]);  
+  function resetEverything(){
+    setReset(prev=>!prev);
 
-  console.log(questionList);
+  }
   function addPoint(){
     setPoint(prev=>prev+1);
   }
   console.log(point);
-  const questionElement=questionList.map((question, index)=><Quiz key={index} addPoint={addPoint} question={question} correctAnswer={question.correct_answer} />);
+  const questionElement=questionList.map((question, index)=><Quiz key={index} reset={reset} addPoint={addPoint} question={question} correctAnswer={question.correct_answer} />);
 
   return (
     <div className="App">
       {questionElement}
-      <button>Next</button>
+      <button onClick={resetEverything}>Next</button>
     </div>
   );
 }
