08b6aaee3bbed34dde2695f3d674603fae50685b
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-08-17 17:36:30 +1000

    randomize the answer for each question, return green if user click the correct answer, return red if user click the wrong answer

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import Quiz from "./Component/Quiz"
 export default function App() {
 
   const [questionList, setQuestionList]=React.useState([]);
+  const [point, setPoint]=React.useState(0);
   React.useEffect(()=>{
     fetch("https://opentdb.com/api.php?amount=5&type=multiple")
       .then(res=>res.json())
       .then(data=>setQuestionList(data.results));
 
-  },[]);
+  },[]);  
+
   console.log(questionList);
-  const questionElement=questionList.map((question, index)=><Quiz key={index} question={question} correctAnswer={question.correct_answer} />)
+  function addPoint(){
+    setPoint(prev=>prev+1);
+  }
+  console.log(point);
+  const questionElement=questionList.map((question, index)=><Quiz key={index} addPoint={addPoint} question={question} correctAnswer={question.correct_answer} />);
+
   return (
     <div className="App">
       {questionElement}
--- src/Component/Answer.js
+++ src/Component/Answer.js
@@ -1,9 +1,24 @@
 import React from "react";
 
 export default function Answer(props){
+    const [singleAnswer, setSingleAnswer]= React.useState(false);
+    const [colorState, setColorState]= React.useState("");
+    function hitAnswer(){
+
+        setSingleAnswer(prev=>!prev);
+        if(props.isHit===false) {
+            props.checkCorrectAnswer();
+            if(props.answer===props.correctAnswer){
+                setColorState("green");
+            }
+            else{
+                setColorState("red");
+            }
+        }
+    }
 
     return (
-        <div className="answerBox">
+        <div style={{backgroundColor: colorState}} onClick={hitAnswer} className="answerBox">
             {props.answer}
         </div>
     )
